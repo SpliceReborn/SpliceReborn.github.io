4fd66326eff54d1a5aac57bed37f96838278ae9b
Darren Seet <splicereborn@gmail.com>
2025-04-21 19:54:55 +0800

    Remove old site files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/SpliceReborn.github.io' : '',
   images: {
     unoptimized: true,
   },
